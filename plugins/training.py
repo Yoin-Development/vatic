a8ad76bbe1fb13edb04b8481deab2c133e07d7ea
Richard Torenvliet <rich.riz@gmail.com>
2014-12-23 16:52:24 +0100

    output opencv createsamples

--- plugins/training.py
+++ plugins/training.py
@@ -24,11 +24,24 @@ class createsample(DumpCommand):
         parser.add_argument("--number_samples", default=10, type=int)
         parser.add_argument("--x", default=128, type=int)
         parser.add_argument("--y", default=128, type=int)
+        parser.add_argument("--jump_frames", default=1, type=int)
+        parser.add_argument("--dimensions", default=False, type=str)
         parser.add_argument("--no-resize", action="store_true", default=False)
         parser.add_argument("--positive", action="store_true", default=False)
         parser.add_argument("--negative", action="store_true", default=False)
         parser.add_argument("--rotate", action="store_true", default=False)
         return parser
+
+    def resize_with_dims(self, box, args):
+        scale = args.scale
+        w, h = tuple(map(float, args.dimensions.split("x")))
+
+        s = w / video.width
+
+        if s * video.height > h:
+            s = h / video.height
+        scale = s
+        return box.transform(scale)
 
     def generate_random_box(self, max_width, max_height, rect_dims, frame):
         w, h = rect_dims
@@ -43,25 +56,45 @@ class createsample(DumpCommand):
                     box.percentoverlap(box_tmp) > 0.05:
                 box_list.pop(i)
 
-    def save_box(self, output_dir, box, image, rect_dims, frame_number, rotate=0):
-        w, h = tuple(map(lambda x: int(math.ceil(x / 2)), rect_dims))
+    def write_to_file(self, output_file, output_list):
+        output_file = open(output_file, "w+")
+        output_file.write("\n".join(output_list))
+        output_file.close()
 
-        box_x, box_y = box.center
+    def save_box(self, output_dir, box, image, rect_dims, frame_number,
+            rotate=0, crop=False, output_list=None, prefix=""):
+        w, h = box.size
+        xmin, ymin = box.xtl, box.ytl
+        img = image.rotate(rotate)
 
-        xmin, ymin = box_x - w, box_y - h
-        xmax, ymax = box_x + w, box_y + h
+        if crop:
+            w_half, h_half = tuple(map(lambda x: int(math.ceil(x / 2)), rect_dims))
+            box_x, box_y = box.center
+            xmin, ymin = box_x - w_half, box_y - h_half
+            xmax, ymax = box_x + w_half, box_y + h_half
+            img = image.crop((xmin, ymin, xmax, ymax))
 
-        image = image.rotate(rotate)
-        img = image.crop((xmin, ymin, xmax, ymax))
-        img.save("{0}/{1}_{2}.jpg".format(output_dir, frame_number, rotate))
+        filename = "{output_dir}/{frame_number}_{x}_{y}_{w}_{h}.jpg".format(
+                        output_dir=output_dir, frame_number=frame_number,
+                        x=xmin, y=ymin, w=w, h=h)
 
-    def create_negatives(self, video, data, args, skipframes=20, rect_dims=(128, 128)):
+        if output_list is not None:
+            # opencvsampletraining outputfile
+            output_string = "{prefix}{filename} {num_objects} {xmin} {ymin} {w} {h}".format(
+                                prefix=prefix, filename=os.path.basename(filename),
+                                num_objects=1, xmin=xmin, ymin=ymin,
+                                w=w, h=h)
+            output_list.append(output_string)
+
+        img.save(filename)
+
+    def create_negatives(self, video, data, args, jump_frames=1, rect_dims=(128, 128)):
         output_dir = "{0}/neg".format(args.output)
         output_filename = "{}/neg_samples.info".format(args.output)
+        output_list = []
 
         try:
             os.makedirs(output_dir)
-            output_file = open(negative_filename, "w+")
         except OSError as e:
             print "Could not create directory: {0}".format(e)
             raise
@@ -72,7 +105,7 @@ class createsample(DumpCommand):
         # get a list of all boxes
         generated_boxes = []
 
-        for frame in range(total_frames):
+        for frame in range(0, total_frames, jump_frames):
             generated_boxes.append(self.generate_random_box(video.width,
                 video.height, rect_dims, frame))
 
@@ -85,86 +118,98 @@ class createsample(DumpCommand):
             frame_number = gen_box.frame
             image = video[frame_number]
 
-            self.save_box(args.output, gen_box, image, rect_dims, frame_number,
-                    rotate=0)
+            self.save_box(output_dir, gen_box, image, rect_dims, frame_number,
+                    rotate=0, crop=True, output_list=output_list, prefix="neg/")
 
             if args.rotate:
                 for deg in [90, 180, 270]:
-                    self.save_box(output_dir, gen_box, image, 
-                            frame_number, rotate=deg)
+                    self.save_box(output_dir, gen_box, image, frame_number, 
+                            crop=True, rotate=deg, output_list=output_list,
+                            prefix="neg/")
 
-        output_file.close()
+        self.write_to_file(output_filename, output_list)
 
-    def create_positives(self, video, data, args, skipframes=20,
+    def create_positives(self, video, data, args, jump_frames=1,
             rect_dims=(128, 128)):
-        output_dir = "{0}/pos".format(args.output)
+        output_dir = "{0}/samples".format(args.output)
         output_filename = "{}/pos_samples.info".format(args.output)
+        output_list = []
 
         try:
             os.makedirs(output_dir)
-            output_file = open(output_filename, "w+")
         except OSError as e:
             print "Could not create directory: {0}".format(e)
             raise
 
         for id, track in enumerate(data):
-            for box in track.boxes:
-                x, y, w, h = [0, 0, 0, 0]
+            for i in range(0, len(track.boxes), jump_frames):
+                box = track.boxes[i]
 
-                if box.occluded > 0:
+                if box.occluded > 0 or box.lost > 0:
                     continue
 
-                if box.lost > 0:
-                    continue
+                # example use: resize box by dimensions of original video
+                if args.dimensions:
+                    box = resize_with_dims(box, args)
 
-                if box.width > box.height:
-                    # Resize height
-                    x = box.xtl
-                    y = box.center[1] - box.width/2
-                    w = box.width
-                    h = box.width
-
-                elif box.height > box.width:
-                    # Resize width
-                    x = box.center[0] - box.height/2
-                    y = box.ytl
-                    w = box.height
-                    h = box.height
-                else:
-                    # Resize whole
-                    x = box.xtl
-                    y = box.ytl
-                    w = box.width
-                    h = box.height
-
-                while (x + w) > video.width:
-                    x-=1
-
-                while x < 0:
-                    x+=1
-
-                while (y + h) > video.height:
-                    y-=1
-
-                while y < 0:
-                    y+=1
-
-                degrees = 0
-                #cx = x + w / 2
-                #cy = y + h / 2
-                #sq = int(math.ceil(math.sqrt(w**2+h**2)))
-                image = video[box.frame]
-                #image = video[box.frame].crop((cx - sq / 2, cy - sq / 2,
-                #                        cx + sq / 2, cy + sq / 2))
                 frame_number = box.frame
+                image = video[frame_number]
 
-                self.save_box(output_dir, box, image, rect_dims, frame_number,
-                        rotate=0)
+                self.save_box(output_dir, box, image, rect_dims,
+                        frame_number, rotate=0, crop=False,
+                        output_list=output_list, prefix="sample/")
 
                 if args.rotate:
                     for deg in [90, 180, 270]:
-                        self.save_box(output_dir, box, image, rect_dims,
-                                frame_number, rotate=deg)
+                        self.save_box(output_dir, tmp_box, image, rect_dims,
+                                frame_number, rotate=deg, crop=False,
+                                output_list=output_list, prefix="sample/")
+
+        self.write_to_file(output_filename, output_list)
+
+        #box_w = box.width
+        #box_h = box.height
+        #box_x, box_y = box.center
+
+        # Resize height
+        #if box.width > box.height:
+        #    x = box.xtl
+        #    y = box.center[1] - box.width / 2
+        #    w = box.width
+        #    h = box.width
+        ## Resize width
+        #elif box.height > box.width:
+        #    x = box.center[0] - box.height / 2
+        #    y = box.ytl
+        #    w = box.height
+        #    h = box.height
+        #else:
+        #    # Resize whole
+        #    x = box.xtl
+        #    y = box.ytl
+        #    w = box.width
+        #    h = box.height
+        #while (x + w) > video.width:
+        #    x-=1
+
+        #while x < 0:
+        #    x+=1
+
+        #while (y + h) > video.height:
+        #    y-=1
+
+        #while y < 0:
+        #    y+=1
+
+        #degrees = 0
+        #cx = x + w / 2
+        #cy = y + h / 2
+        #sq = int(math.ceil(math.sqrt(w**2 + h**2)))
+        #tmp_box = Box(x, y, x + w, y + h)
+
+        #image = video[box.frame]
+        #image = video[box.frame].crop((cx - sq / 2, cy - sq / 2,
+        #                        cx + sq / 2, cy + sq / 2))
 
     def __call__(self, args):
         try:
@@ -179,9 +224,11 @@ class createsample(DumpCommand):
 
         if args.negative:
             rect_dims = args.x, args.y
-            self.create_negatives(video, data, args, rect_dims=rect_dims)
+            self.create_negatives(video, data, args, rect_dims=rect_dims,
+                    jump_frames=args.jump_frames)
 
         # prepend class label
         if args.positive:
             rect_dims = args.x, args.y
-            self.create_positives(video, data, args, rect_dims=rect_dims)
+            self.create_positives(video, data, args, rect_dims=rect_dims, 
+                    jump_frames=args.jump_frames)
